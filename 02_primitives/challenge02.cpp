2f19e64f1cda00cd7d07ea2886cc82f215382055
Muhammad Danish <Muhammad.danish1015@gmail.com>
2025-11-14 12:32:18 +0500

    Basic Challenge 02

--- 02_primitives/challenge02.cpp
+++ 02_primitives/challenge02.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
 int main()
 {
-   
+    float basePrice;
+
+    cout << "Enter the Base Price of Tea: ";
+    cin >> basePrice;
+
+    float increasedPrice = basePrice * 0.10;
+    float newPrice = basePrice + increasedPrice;
+
+    int roundedPrice = (int)newPrice;
+
+    cout << "Base Price of Tea is: " << basePrice << endl;
+    cout << "New Price after 10% increase: " << newPrice << endl;
+    cout << "Rounded New Price: " << roundedPrice << endl;
 
     return 0;
 }
