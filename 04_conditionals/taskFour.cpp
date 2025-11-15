53bb43fb32d5a67c243e791603ed4c911446a6ed
Muhammad Danish <Muhammad.danish1015@gmail.com>
2025-11-15 21:24:40 +0500

    Conditional task four
    Switch

---
+++ 04_conditionals/taskFour.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int choice;
+    double price;
+
+    cout << "Select your tea\n";
+    cout << "1. Green Tea\n";
+    cout << "2. Lemon Tea\n";
+    cout << "3. Masala Tea\n";
+    cout << "Enter your choice in number: \n";
+    cin >> choice;
+
+    switch (choice)
+    {
+    case 1:
+        price = 150;
+        cout << "You selected Green Tea. Price " << price << endl;
+        break;
+    case 2:
+        price = 100;
+        cout << "You selected Lemon Tea. Price " << price << endl;
+        break;
+    case 3:
+        price = 70;
+        cout << "You selected Masala Tea. Price " << price << endl;
+        break;
+    default:
+        cout << "Invalid Choice" << endl;
+        break;
+    }
+
+    return 0;
+}
