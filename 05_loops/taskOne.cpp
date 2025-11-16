c4f7f8c1c57a250c23bf94248d1894c8c300647e
Muhammad Danish <Muhammad.danish1015@gmail.com>
2025-11-16 18:33:07 +0500

    While Loop

---
+++ 05_loops/taskOne.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int teaCups;
+
+    cout << "Enter the number of tea cups to serve: ";
+    cin >> teaCups;
+
+    // while loop
+
+    while (teaCups > 0)
+    {
+        teaCups--;
+        cout << "Serving a cup of tea \n"
+             << teaCups << " Remainig" << endl;
+    }
+
+    cout << "All tea cups are served." << endl;
+}
