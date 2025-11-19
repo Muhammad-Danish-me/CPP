c4301408e8f7883965fc7c44f7fc01fb30829efd
Muhammad Danish <Muhammad.danish1015@gmail.com>
2025-11-19 18:37:11 +0500

    Loop with Break keyword

---
+++ 05_loops/taskFour.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string response;
+    while (true)
+    {
+        cout << "Do you want more tea (type 'stop' to exit)? ";
+        getline(cin, response);
+
+        if (response == "stop")
+        {
+            // exit the loop
+            cout << "Thanks For ordering";
+            break;
+        }
+
+        cout << "Here is your another cup of tea." << endl;
+    }
+
+    return 0;
+}
